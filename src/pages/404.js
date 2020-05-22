bdddb487cce26d34e58b61f4f289157c784f896f
Miguel Tejeda <tejedamiguel6@aol.com>
2020-05-22 13:03:55 -0400

    netlify hooks

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,8 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from "../components/HomePage/Layout"
-import Head from "../components/HomePage/Head"
+import Layout from '../components/HomePage/Layout'
+import Head from '../components/HomePage/Head'
 
 const NotFound = () => {
   return (
